42a31e78651f87b93d0089102bd7ff5a41af4769
Ania <anna.kozak61@gmail.com>
2023-12-01 17:24:02 +0100

    Changes to the Wall file

--- src/main/java/org/example/model/Wall.java
+++ src/main/java/org/example/model/Wall.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class Wall extends SingleBlock implements Structure, CompositeBlock {
+public class Wall extends SingleBlock implements Structure{
 
     private List<Block> blocks;
 
